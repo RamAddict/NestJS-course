7644d324a95771efa0cf49c9ee83e40b00fd52e8
ramaddict <apbiancofriber@gmail.com>
2022-10-02 18:27:50 -0300

    add more users controller tests

--- mycv/src/users/users.controller.spec.ts
+++ mycv/src/users/users.controller.spec.ts
@@ -14,7 +14,11 @@ describe('UsersController', () => {
             findOne: (id: number) => Promise.resolve({ id, email: 'cancain@sad.csa', password: 'sad!' } as User),
             find: (email: string) => Promise.resolve([{ id: 1, email, password: 'sad!' } as User]),
         };
-        fakeAuthService = {};
+        fakeAuthService = {
+            signIn: (email: string, password: string) => {
+                return Promise.resolve({id: 1, email, password} as User);
+            }
+        };
         const module: TestingModule = await Test.createTestingModule({
             controllers: [UsersController],
             providers: [
@@ -29,4 +33,23 @@ describe('UsersController', () => {
     it('should be defined', () => {
         expect(controller).toBeDefined();
     });
+    it('findAllUsers returns a list of users with the given email', async () => {
+        const users = await controller.findAllUsers('cancain@sad.csa');
+        expect(users.length).toEqual(1);
+        expect(users[0].email).toEqual('cancain@sad.csa');
+    });
+    it('findUser returns a single user with the given id', async () => {
+        const user = await controller.findUser('1');
+        expect(user).toBeDefined();
+    });
+    it('findUser throws an error if user with given id is not found', async () => {
+        fakeUsersService.findOne = () => null;
+        await expect(controller.findUser('1')).rejects.toThrow();
+    });
+    it('signin updates session object and returns user', async () => {
+        const session = {userId: 11};
+        const user = await controller.signIn({email: "canc@cnas.a", password: "sad"}, session);
+        expect(user.id).toEqual(1);
+        expect(session.userId).toEqual(1);
+    })
 });
