10a5bb0cd326f8ddb2d6a29dbf841216bb1f24a6
ramaddict <apbiancofriber@gmail.com>
2022-10-22 20:09:07 -0300

    Fix typesccript type issue

--- mycv/src/users/middlewares/current-user.middleware.ts
+++ mycv/src/users/middlewares/current-user.middleware.ts
@@ -1,10 +1,18 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { User } from '../user.entity';
 import { UsersService } from '../users.service';
+
+declare global {
+    interface Request {
+        currentUser?: User;
+        session?: {userId?: number};
+    }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
     constructor(private usersService: UsersService) {}
-    async use(req: any, res: Response, next: () => void) {
+    async use(req: Request, res: Response, next: () => void) {
         const {userId} = req.session || {};
         if (userId) {
             const user = await this.usersService.findOne(userId);
